e599996a3f6002d85d811332ff7d057675dd6c4d
useranonymous00101 <khatrirohan430@gmail.com>
2024-03-10 18:39:52 +0545

    all submitted

---
+++ Lab_Sheet_03.c
@@ -0,0 +1,60 @@
+/*
+    Author: Rohan khatri
+    Description: C -Assignments B.Sc CSIT 1st Semester
+*/
+
+// Questions from 31 - 36 are available here...
+
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    // Q no. 31
+    // char name;
+    // printf("Enter a character: ");
+    // scanf("%c", &name);
+    // printf("You entered %c", name);
+    // printf("\nThe ASCII value is %d", name);
+
+    // // Q no 32
+    // char user;
+    // printf("Enter a char: ");
+    // user = getchar();
+    // printf("You entered: ");
+    // putchar(user);
+
+    // // Q no 33
+    // char string[20];
+    // printf("Enter a string: ");
+    // gets(string);
+    // printf("You entered: ");
+    // puts(string);
+
+    // // Q no 34
+    // int integer = 10;
+    // float float_point = 121.12;
+    // char character = 'B';
+
+    // printf("Integer: %d", integer);
+    // printf("\nFloating_Point: %.2f", float_point);
+    // printf("\nCharacter: %c", character);
+
+    // Q no 35
+
+    // Already done in Q no. 31
+
+    // // Q no 36
+    // int id;
+    // char userName[100];
+    // int salary;
+    // printf("Enter Id: ");
+    // scanf("%d", &id);
+    // printf("Enter username: ");
+    // scanf("%s", userName);
+    // printf("Enter salary: ");
+    // scanf("%d", &salary);
+
+    // printf("ID = %d\nUserName = %s\nSalary = %d", id, userName, salary);
+
+    return 0;
+}
---
+++ Lab_Sheet_04.c
@@ -0,0 +1,6 @@
+/*
+    Author: Rohan khatri
+    Description: C -Assignments B.Sc CSIT 1st Semester
+*/
+
+// Questions from 37 - 43 are available here...
